482cc60b546777e86e0d61552843d811377c7542
sangitha1 <mensinghs@gmail.com>
2015-11-25 20:42:22 +0000

    updates

--- config/database.rb
+++ config/database.rb
@@ -17,3 +17,4 @@ configure do
   end
 
 end
+
--- db/migrate/0_create_base_tables.rb
+++ db/migrate/0_create_base_tables.rb
@@ -6,26 +6,26 @@ class CreateBaseTables < ActiveRecord::Migration
       t.string :avatar_url
       t.string :email
       t.string :password
-      t.timestamps
+      t.timestamps null: false
     end
 
     create_table :posts do |t|
       t.references :user
       t.string :photo_url
-      t.timestamps
+      t.timestamps null: false
     end
 
     create_table :comments do |t|
       t.references :user
       t.references :post
       t.text :text
-      t.timestamps
+      t.timestamps null: false
     end
 
     create_table :likes do |t|
       t.references :user
       t.references :post
-      t.timestamps
+      t.timestamps null: false
     end
 
   end
